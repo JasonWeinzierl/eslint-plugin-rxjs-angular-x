a4f97fb46d8b23041b30878c5d759c03cdf8b288
Jason Weinzierl <weinzierljason@gmail.com>
2025-01-27 14:46:02 -0600

    refactor: rename source dir to src

--- tests/rules/prefer-async-pipe.ts
+++ tests/rules/prefer-async-pipe.ts
@@ -1,6 +1,6 @@
 import { stripIndent } from "common-tags";
 import { fromFixture } from "eslint-etc";
-import rule = require("../../source/rules/prefer-async-pipe");
+import rule = require("../../src/rules/prefer-async-pipe");
 import { ruleTester } from "../utils";
 
 ruleTester({ types: true }).run("prefer-async-pipe", rule, {

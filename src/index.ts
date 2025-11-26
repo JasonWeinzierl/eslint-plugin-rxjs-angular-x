e0ac67e781f348cba406c38a725496dfeb9d5b14
Jason Weinzierl <weinzierljason@gmail.com>
2025-11-26 10:52:14 -0600

    fix: compatibility with eslint's defineConfig (#16)
    Currently there is an incompatibility between eslint's types and
    tslint's, especially since we use tseslint's RuleCreator
    (typescript-eslint issue 11543).
    
    - Fixed compatibility with `defineConfig()` so it works in addition to
    the deprecated `tseslint.config()`.
    - The plugin now satisfies `ESLint.Plugin` instead of
    `TSESLint.FlatConfig.Plugin`.
    	- The rules satisfy _both_ `Rule.RuleModule` and `TSESLint.RuleModule`.
    - Improved the compiled types to include inlined literal docs (e.g.
    `description` in intellisense is the actual rule description instead of
    just `string`).
    - Internally upgraded our own eslint config to `defineConfig`.
    - Bumped the vitest eslint plugin to 1.4.0 to be compatible with
    defineConfig.
    	- Pinned `@eslint/config-helpers` because 0.4.2 causes type errors.
    - Removed references to `tseslint.config` from the README.
    - Re-wrote the migration section of the README to include exact steps
    required.
    
    This imitates a similar change made here:
    https://github.com/JasonWeinzierl/eslint-plugin-rxjs-x/pull/283

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
+import type { TSESLint } from '@typescript-eslint/utils';
+import type { ESLint, Rule } from 'eslint';
 import { name, version } from '../package.json';
 import { preferAsyncPipeRule } from './rules/prefer-async-pipe';
 import { preferCompositionRule } from './rules/prefer-composition';
 import { preferTakeuntilRule } from './rules/prefer-takeuntil';
 
+const allRules = {
+  'prefer-async-pipe': preferAsyncPipeRule,
+  'prefer-composition': preferCompositionRule,
+  'prefer-takeuntil': preferTakeuntilRule,
+} satisfies TSESLint.FlatConfig.Plugin['rules'];
+
 const rxjsAngularX = {
   meta: { name, version },
-  rules: {
-    'prefer-async-pipe': preferAsyncPipeRule,
-    'prefer-composition': preferCompositionRule,
-    'prefer-takeuntil': preferTakeuntilRule,
-  },
-};
+  /** Compatibility with `defineConfig` until https://github.com/typescript-eslint/typescript-eslint/issues/11543 is addressed. */
+  rules: allRules as { [K in keyof typeof allRules]: (typeof allRules)[K] & Rule.RuleModule },
+} satisfies ESLint.Plugin;
 
 export default rxjsAngularX;

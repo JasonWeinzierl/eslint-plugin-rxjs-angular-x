9aafa38a09438ccf7a79c80d6eb1a6a25ea85ff2
Jason Weinzierl <weinzierljason@gmail.com>
2026-02-16 10:31:11 -0600

    chore: use rolldown for build and test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import type { TSESLint } from '@typescript-eslint/utils';
 import type { ESLint, Rule } from 'eslint';
-import { name, version } from '../package.json';
+import packageJson from '../package.json' with { type: 'json' };
 import { preferAsyncPipeRule } from './rules/prefer-async-pipe';
 import { preferCompositionRule } from './rules/prefer-composition';
 import { preferTakeuntilRule } from './rules/prefer-takeuntil';
@@ -13,8 +13,8 @@ const allRules = {
 
 const rxjsAngularX = {
   meta: {
-    name,
-    version,
+    name: packageJson.name,
+    version: packageJson.version,
     namespace: 'rxjs-angular-x',
   },
   /** Compatibility with `defineConfig` until https://github.com/typescript-eslint/typescript-eslint/issues/11543 is addressed. */

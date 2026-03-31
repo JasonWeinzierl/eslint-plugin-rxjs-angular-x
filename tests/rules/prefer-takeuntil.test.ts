JasonWeinzierl/eslint-plugin-rxjs-angular-x

commit 2698833ad3ffeac7cc63478ba6469c9040845bc6

feat!: ESLint v10 (#30)
BREAKING CHANGES: ESLint v10 is required; Node.js v18 is no longer
supported; ESM is required.

1. Adds support for ESLint v10, drops support for v8 and v9.
2. Bumps Node.js supported versions to align with ESLint v10.
3. Drops support for CommonJS; see
https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c
for more info.
4. `docs` folder is no longer published to npm.
5. Default options use the new `meta.defaultOptions` field.
6. Internally use rolldown for build and test.
7. `common-tags` is no longer required.
8. `ts-api-utils` ^2.4.0 is required.
9. `@typescript-eslint/utils` ^8.56.0 is required.
10. Delete unused code; adjust code coverage thresholds accordingly.

Resolves #27

src/rules/prefer-takeuntil.ts
@@ -2,7 +2,6 @@ import {
   TSESTree as es,
   TSESLint,
 } from '@typescript-eslint/utils';
-import { stripIndent } from 'common-tags';
 import {
   getTypeServices,
   isCallExpression,
@@ -24,16 +23,15 @@ type MessageIds = keyof typeof messages;
 const ngOnDestroyMethodSelector
   = 'MethodDefinition[key.name=\'ngOnDestroy\'][kind=\'method\']';
 
-const defaultOptions: readonly {
+type Options = [{
   alias?: string[];
   checkComplete?: boolean;
   checkDecorators?: string[];
   checkDestroy?: boolean;
   superClass?: string[];
-}[] = [];
+}];
 
 export const preferTakeuntilRule = ruleCreator({
-  defaultOptions,
   meta: {
     docs: {
       description:
@@ -53,17 +51,22 @@ export const preferTakeuntilRule = ruleCreator({
           superClass: { type: 'array', items: { type: 'string' }, description: 'An optional array of superclass names that already implement a `Subject`-based `ngOnDestroy`' },
         },
         type: 'object',
-        description: stripIndent`
-        An optional object with optional \`alias\`, \`checkComplete\`, \`checkDecorators\` and \`checkDestroy\` properties.
-        The \`alias\` property is an array containing the names of operators that aliases for \`takeUntil\`.
-        The \`checkComplete\` property is a boolean that determines whether or not \`complete\` must be called after \`next\`.
-        The \`checkDecorators\` property is an array containing the names of the decorators that determine whether or not a class is checked.
-        The \`checkDestroy\` property is a boolean that determines whether or not a \`Subject\`-based \`ngOnDestroy\` must be implemented.
-        The \`superClass\` property is an array containing the names of classes to extend from that already implement a \`Subject\`-based \`ngOnDestroy\`.
-      `,
+        description: `An optional object with optional \`alias\`, \`checkComplete\`, \`checkDecorators\` and \`checkDestroy\` properties.
+The \`alias\` property is an array containing the names of operators that aliases for \`takeUntil\`.
+The \`checkComplete\` property is a boolean that determines whether or not \`complete\` must be called after \`next\`.
+The \`checkDecorators\` property is an array containing the names of the decorators that determine whether or not a class is checked.
+The \`checkDestroy\` property is a boolean that determines whether or not a \`Subject\`-based \`ngOnDestroy\` must be implemented.
+The \`superClass\` property is an array containing the names of classes to extend from that already implement a \`Subject\`-based \`ngOnDestroy\`.`,
       },
     ],
     type: 'problem',
+    defaultOptions: [{
+      alias: [],
+      checkComplete: false,
+      checkDecorators: ['Component'],
+      checkDestroy: true,
+      superClass: [],
+    }] as Options,
   },
   name: 'prefer-takeuntil',
   create: (context) => {
@@ -73,14 +76,13 @@ export const preferTakeuntilRule = ruleCreator({
     // it's explicitly configured. It's extremely unlikely a subject-based
     // destroy mechanism will be used in conjunction with an alias.
 
-    const [config = {}] = context.options;
-    const {
+    const [{
       alias = [],
-      checkComplete = false,
-      checkDecorators = ['Component'],
-      checkDestroy = alias.length === 0,
+      checkComplete,
+      checkDecorators = [],
+      checkDestroy,
       superClass = [],
-    } = config;
+    }] = context.options;
 
     interface Entry {
       classDeclaration: es.ClassDeclaration;

.eslint-doc-generatorrc.js
@@ -1,0 +1,12 @@
+// @ts-check
+
+/** @type {import('eslint-doc-generator').GenerateOptions} */
+export default {
+  ruleDocTitleFormat: 'desc-parens-prefix-name',
+  ruleDocNotices: [
+    'deprecated',
+    'configs',
+    'fixableAndHasSuggestions',
+    'requiresTypeChecking',
+  ],
+};

.github/workflows/ci.yml
@@ -21,7 +21,6 @@ jobs:
     strategy:
       matrix:
         node-version:
-          - 18
           - 20
           - 22
           - 24

.github/workflows/report-coverage.yml
@@ -28,4 +28,4 @@ jobs:
       - name: Report Coverage
         uses: davelosert/vitest-coverage-report-action@v2
         with:
-          threshold-icons: "{0: '🔴', 80: '🟠', 90: '🟢'}"
+          threshold-icons: "{0: '🔴', 70: '🟠', 85: '🟢'}"

eslint.config.js
@@ -78,10 +78,6 @@ export default defineConfig(gitignore(), {
         require: 'always',
       },
     ],
-    // TODO: fixing these may require bumping the minimum ESLint version.
-    'eslint-plugin/require-meta-default-options': 'off',
-    'eslint-plugin/no-meta-schema-default': 'off',
-    'eslint-plugin/no-meta-replaced-by': 'off',
 
     '@typescript-eslint/no-unnecessary-condition': 'off',
     '@typescript-eslint/restrict-template-expressions': [

src/etc/could-be-type.ts
@@ -4,29 +4,25 @@ import ts from 'typescript';
 export function couldBeType(
   type: ts.Type,
   name: string | RegExp,
-  qualified?: {
-    name: RegExp;
-    typeChecker: ts.TypeChecker;
-  },
 ): boolean {
   if (tsutils.isTypeReference(type)) {
     type = type.target;
   }
 
-  if (isType(type, name, qualified)) {
+  if (isType(type, name)) {
     return true;
   }
 
   if (tsutils.isUnionOrIntersectionType(type)) {
-    return type.types.some(t => couldBeType(t, name, qualified));
+    return type.types.some(t => couldBeType(t, name));
   }
 
   const baseTypes = type.getBaseTypes();
-  if (baseTypes?.some(t => couldBeType(t, name, qualified))) {
+  if (baseTypes?.some(t => couldBeType(t, name))) {
     return true;
   }
 
-  if (couldImplement(type, name, qualified)) {
+  if (couldImplement(type, name)) {
     return true;
   }
 
@@ -36,20 +32,8 @@ export function couldBeType(
 function isType(
   type: ts.Type,
   name: string | RegExp,
-  qualified?: {
-    name: RegExp;
-    typeChecker: ts.TypeChecker;
-  },
 ): boolean {
   if (!type.symbol) {
-    return false;
-  }
-  if (
-    qualified
-    && !qualified.name.test(
-      qualified.typeChecker.getFullyQualifiedName(type.symbol),
-    )
-  ) {
     return false;
   }
   return typeof name === 'string'
@@ -60,10 +44,6 @@ function isType(
 function couldImplement(
   type: ts.Type,
   name: string | RegExp,
-  qualified?: {
-    name: RegExp;
-    typeChecker: ts.TypeChecker;
-  },
 ): boolean {
   const { symbol } = type;
   if (symbol) {
@@ -74,7 +54,7 @@ function couldImplement(
         const implemented = heritageClauses.some(
           ({ token, types }) =>
             token === ts.SyntaxKind.ImplementsKeyword
-            && types.some(node => isMatchingNode(node, name, qualified)),
+            && types.some(node => isMatchingNode(node, name)),
         );
         if (implemented) {
           return true;
@@ -88,23 +68,8 @@ function couldImplement(
 function isMatchingNode(
   node: ts.ExpressionWithTypeArguments,
   name: string | RegExp,
-  qualified?: {
-    name: RegExp;
-    typeChecker: ts.TypeChecker;
-  },
 ): boolean {
   const { expression } = node;
-  if (qualified) {
-    const type = qualified.typeChecker.getTypeAtLocation(expression);
-    if (type) {
-      const qualifiedName = qualified.typeChecker.getFullyQualifiedName(
-        type.symbol,
-      );
-      if (!qualified.name.test(qualifiedName)) {
-        return false;
-      }
-    }
-  }
   const text = expression.getText();
   return typeof name === 'string' ? text === name : Boolean(text.match(name));
 }

src/etc/get-type-services.ts
@@ -6,19 +6,16 @@ export function getTypeServices<
   TOptions extends readonly unknown[],
 >(context: TSESLint.RuleContext<TMessageIds, Readonly<TOptions>>) {
   const services = ESLintUtils.getParserServices(context);
-  const { program, getTypeAtLocation } = services;
-  const typeChecker = program.getTypeChecker();
+  const { getTypeAtLocation } = services;
 
   const couldBeType = (
     node: TSESTree.Node,
     name: string | RegExp,
-    qualified?: { name: RegExp },
   ): boolean => {
     const type = getTypeAtLocation(node);
     return tsutilsEtcCouldBeType(
       type,
       name,
-      qualified ? { ...qualified, typeChecker } : undefined,
     );
   };
 

src/etc/is.ts
@@ -1,147 +1,25 @@
 import { AST_NODE_TYPES, TSESTree } from '@typescript-eslint/utils';
 
-export function hasTypeAnnotation<T extends TSESTree.Node>(
-  node: T,
-): node is T & { typeAnnotation: TSESTree.TSTypeAnnotation } {
-  return 'typeAnnotation' in node && !!node.typeAnnotation;
-}
-
-export function isArrayExpression(node: TSESTree.Node): node is TSESTree.ArrayExpression {
-  return node.type === AST_NODE_TYPES.ArrayExpression;
-}
-
-export function isArrayPattern(node: TSESTree.Node): node is TSESTree.ArrayPattern {
-  return node.type === AST_NODE_TYPES.ArrayPattern;
-}
-
-export function isArrowFunctionExpression(
-  node: TSESTree.Node,
-): node is TSESTree.ArrowFunctionExpression {
-  return node.type === AST_NODE_TYPES.ArrowFunctionExpression;
-}
-
 export function isAssignmentExpression(
   node: TSESTree.Node,
 ): node is TSESTree.AssignmentExpression {
   return node.type === AST_NODE_TYPES.AssignmentExpression;
 }
 
-export function isBlockStatement(node: TSESTree.Node): node is TSESTree.BlockStatement {
-  return node.type === AST_NODE_TYPES.BlockStatement;
-}
-
 export function isCallExpression(node: TSESTree.Node): node is TSESTree.CallExpression {
   return node.type === AST_NODE_TYPES.CallExpression;
-}
-
-export function isChainExpression(node: TSESTree.Node): node is TSESTree.ChainExpression {
-  return node.type === AST_NODE_TYPES.ChainExpression;
-}
-
-export function isExportNamedDeclaration(
-  node: TSESTree.Node,
-): node is TSESTree.ExportNamedDeclaration {
-  return node.type === AST_NODE_TYPES.ExportNamedDeclaration;
-}
-
-export function isExpressionStatement(
-  node: TSESTree.Node,
-): node is TSESTree.ExpressionStatement {
-  return node?.type === AST_NODE_TYPES.ExpressionStatement;
-}
-
-export function isFunctionDeclaration(
-  node: TSESTree.Node,
-): node is TSESTree.FunctionDeclaration {
-  return node.type === AST_NODE_TYPES.FunctionDeclaration;
-}
-
-export function isFunctionExpression(
-  node: TSESTree.Node,
-): node is TSESTree.FunctionExpression {
-  return node.type === AST_NODE_TYPES.FunctionExpression;
 }
 
 export function isIdentifier(node: TSESTree.Node): node is TSESTree.Identifier {
   return node.type === AST_NODE_TYPES.Identifier;
 }
 
-export function isImportDeclaration(node: TSESTree.Node): node is TSESTree.ImportDeclaration {
-  return node.type === AST_NODE_TYPES.ImportDeclaration;
-}
-
-export function isImportNamespaceSpecifier(node: TSESTree.Node): node is TSESTree.ImportNamespaceSpecifier {
-  return node.type === AST_NODE_TYPES.ImportNamespaceSpecifier;
-}
-
-export function isImportSpecifier(node: TSESTree.Node): node is TSESTree.ImportSpecifier {
-  return node.type === AST_NODE_TYPES.ImportSpecifier;
-}
-
-export function isJSXExpressionContainer(node: TSESTree.Node): node is TSESTree.JSXExpressionContainer {
-  return node.type === AST_NODE_TYPES.JSXExpressionContainer;
-}
-
-export function isLiteral(node: TSESTree.Node): node is TSESTree.Literal {
-  return node.type === AST_NODE_TYPES.Literal;
-}
-
 export function isMemberExpression(node: TSESTree.Node): node is TSESTree.MemberExpression {
   return node.type === AST_NODE_TYPES.MemberExpression;
 }
 
-export function isMethodDefinition(node: TSESTree.Node): node is TSESTree.MethodDefinition {
-  return node.type === AST_NODE_TYPES.MethodDefinition;
-}
-
-export function isNewExpression(node: TSESTree.Node): node is TSESTree.NewExpression {
-  return node.type === AST_NODE_TYPES.NewExpression;
-}
-
-export function isObjectExpression(node: TSESTree.Node): node is TSESTree.ObjectExpression {
-  return node.type === AST_NODE_TYPES.ObjectExpression;
-}
-
-export function isObjectPattern(node: TSESTree.Node): node is TSESTree.ObjectPattern {
-  return node.type === AST_NODE_TYPES.ObjectPattern;
-}
-
-export function isProgram(node: TSESTree.Node): node is TSESTree.Program {
-  return node.type === AST_NODE_TYPES.Program;
-}
-
-export function isProperty(node: TSESTree.Node): node is TSESTree.Property {
-  return node.type === AST_NODE_TYPES.Property;
-}
-
-export function isPropertyDefinition(node: TSESTree.Node): node is TSESTree.PropertyDefinition {
-  return node.type === AST_NODE_TYPES.PropertyDefinition;
-}
-
-export function isPrivateIdentifier(
-  node: TSESTree.Node,
-): node is TSESTree.PrivateIdentifier {
-  return node.type === AST_NODE_TYPES.PrivateIdentifier;
-}
-
-export function isRestElement(node: TSESTree.Node): node is TSESTree.RestElement {
-  return node.type === AST_NODE_TYPES.RestElement;
-}
-
 export function isThisExpression(node: TSESTree.Node): node is TSESTree.ThisExpression {
   return node.type === AST_NODE_TYPES.ThisExpression;
-}
-
-export function isTSTypeLiteral(node: TSESTree.Node): node is TSESTree.TSTypeLiteral {
-  return node.type === AST_NODE_TYPES.TSTypeLiteral;
-}
-
-export function isTSTypeReference(node: TSESTree.Node): node is TSESTree.TSTypeReference {
-  return node.type === AST_NODE_TYPES.TSTypeReference;
-}
-
-export function isUnaryExpression(node: TSESTree.Node): node is TSESTree.UnaryExpression {
-  return node.type === AST_NODE_TYPES.UnaryExpression;
 }
 
 export function isVariableDeclarator(

commit e3df134a8b905e11a1778932dc8bbe881fb84bd3

fix: allow aliases that don't accept subjects
This fixes a bug introduced by #30, where checkDestroy was supposed to conditionally default to true when aliases is empty and false when aliases are provided.

Because we can only guarantee takeUntil works for the subject-destroy pattern, only fail a subscribe if the operator is literally named takeUntil.

src/rules/prefer-takeuntil.ts
@@ -14,7 +14,7 @@ import { ruleCreator } from '../utils';
 const messages = {
   noDestroy: '`ngOnDestroy` is not implemented.',
   noTakeUntil:
-    'Forbids calling `subscribe` without an accompanying `takeUntil`.',
+    'Calling `subscribe` without an accompanying `takeUntil` is forbidden.',
   notCalled: '`{{name}}.{{method}}()` not called.',
   notDeclared: 'Subject `{{name}}` not a class property.',
 } as const;
@@ -47,7 +47,7 @@ export const preferTakeuntilRule = ruleCreator({
           alias: { type: 'array', items: { type: 'string' }, description: 'An optional array of operator names that alias for `takeUntil`.' },
           checkComplete: { type: 'boolean', description: 'Check for `complete` calls.' },
           checkDecorators: { type: 'array', items: { type: 'string' }, description: 'An optional array of decorator names to check.' },
-          checkDestroy: { type: 'boolean', description: 'Check for `Subject`-based `ngOnDestroy`.' },
+          checkDestroy: { type: 'boolean', description: 'Check for `Subject`-based `ngOnDestroy`. Defaults to `true` when `alias` is empty and `false` when `alias` is non-empty.' },
           superClass: { type: 'array', items: { type: 'string' }, description: 'An optional array of superclass names that already implement a `Subject`-based `ngOnDestroy`' },
         },
         type: 'object',
@@ -55,7 +55,7 @@ export const preferTakeuntilRule = ruleCreator({
 The \`alias\` property is an array containing the names of operators that aliases for \`takeUntil\`.
 The \`checkComplete\` property is a boolean that determines whether or not \`complete\` must be called after \`next\`.
 The \`checkDecorators\` property is an array containing the names of the decorators that determine whether or not a class is checked.
-The \`checkDestroy\` property is a boolean that determines whether or not a \`Subject\`-based \`ngOnDestroy\` must be implemented.
+The \`checkDestroy\` property is a boolean that determines whether or not a \`Subject\`-based \`ngOnDestroy\` must be implemented. If not set, it defaults to \`true\` when \`alias\` is empty and \`false\` when \`alias\` is non-empty.
 The \`superClass\` property is an array containing the names of classes to extend from that already implement a \`Subject\`-based \`ngOnDestroy\`.`,
       },
     ],
@@ -64,17 +64,13 @@ The \`superClass\` property is an array containing the names of classes to exten
       alias: [],
       checkComplete: false,
       checkDecorators: ['Component'],
-      checkDestroy: true,
+      checkDestroy: undefined,
       superClass: [],
     }] as Options,
   },
   name: 'prefer-takeuntil',
   create: (context) => {
     const { couldBeObservable } = getTypeServices(context);
-
-    // If an alias is specified, check for the subject-based destroy only if
-    // it's explicitly configured. It's extremely unlikely a subject-based
-    // destroy mechanism will be used in conjunction with an alias.
 
     const [{
       alias = [],
@@ -83,6 +79,11 @@ The \`superClass\` property is an array containing the names of classes to exten
       checkDestroy,
       superClass = [],
     }] = context.options;
+
+    // If an alias is specified, check for the subject-based destroy only if
+    // it's explicitly configured. It's extremely unlikely a subject-based
+    // destroy mechanism will be used in conjunction with an alias.
+    const enforceSubjectDestroyPattern = checkDestroy ?? alias.length === 0;
 
     interface Entry {
       classDeclaration: es.ClassDeclaration;
@@ -112,7 +113,7 @@ The \`superClass\` property is an array containing the names of classes to exten
         checkSubscribe(callExpression, entry);
       });
 
-      if (checkDestroy) {
+      if (enforceSubjectDestroyPattern) {
         checkNgOnDestroy(entry);
       }
     }
@@ -227,7 +228,8 @@ The \`superClass\` property is an array containing the names of classes to exten
       if (!isIdentifier(callee)) {
         return { found: false };
       }
-      if (callee.name === 'takeUntil' || alias.includes(callee.name)) {
+      const isTakeUntil = callee.name === 'takeUntil';
+      if (isTakeUntil || alias.includes(callee.name)) {
         const [arg] = callExpression.arguments;
         if (arg) {
           if (
@@ -240,7 +242,7 @@ The \`superClass\` property is an array containing the names of classes to exten
             return { found: true, name: arg.name };
           }
         }
-        if (!checkDestroy) {
+        if (!isTakeUntil || !enforceSubjectDestroyPattern) {
           return { found: true };
         }
       }

tests/rules/prefer-takeuntil.test.ts
@@ -223,6 +223,121 @@ ruleTester({ types: true }).run('prefer-takeuntil', preferTakeuntilRule, {
       options: [{ alias: ['someAlias'] }],
     },
     {
+      name: 'with takeUntilDestroyed alias',
+      code: stripIndent`
+        import { Component, DestroyRef, inject } from "@angular/core";
+        import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+        import { of } from "rxjs";
+        import { switchMap } from "rxjs/operators";
+
+        const o = of("o");
+
+        @Component({
+          selector: "component-with-takeuntil-destroyed"
+        })
+        class CorrectComponent {
+          private readonly destroyRef = inject(DestroyRef);
+
+          someMethod() {
+            o.pipe(
+              switchMap(_ => o),
+              takeUntilDestroyed(this.destroyRef)
+            ).subscribe();
+          }
+        }
+      `,
+      options: [{ alias: ['takeUntilDestroyed'] }],
+    },
+    {
+      name: 'with alias accepting this param',
+      code: stripIndent`
+        import { Component } from "@angular/core";
+        import { of } from "rxjs";
+        import { switchMap } from "rxjs/operators";
+
+        declare const UntilDestroy: ClassDecorator;
+        declare function untilDestroyed(instance: unknown): unknown;
+
+        const observable = of("o");
+
+        @UntilDestroy()
+        @Component({
+          selector: "component-with-until-destroyed"
+        })
+        class MyComponent {
+          ngOnInit() {
+            observable.pipe(
+              switchMap(_ => observable),
+              untilDestroyed(this)
+            ).subscribe();
+          }
+        }
+      `,
+      options: [
+        {
+          alias: ['untilDestroyed', 'untilCompleted'],
+          checkDecorators: ['Component', 'Directive', 'Pipe', 'Injectable'],
+        },
+      ],
+    },
+    {
+      name: 'with alias accepting zero params',
+      code: stripIndent`
+        import { Injectable } from "@angular/core";
+        import { of } from "rxjs";
+        import { switchMap } from "rxjs/operators";
+
+        declare function untilCompleted(): unknown;
+
+        const anotherObservable = of("o");
+
+        @Injectable()
+        class MyService {
+          someMethod() {
+            anotherObservable.pipe(
+              switchMap(_ => anotherObservable),
+              untilCompleted()
+            ).subscribe();
+          }
+        }
+      `,
+      options: [
+        {
+          alias: ['untilDestroyed', 'untilCompleted'],
+          checkDecorators: ['Component', 'Directive', 'Pipe', 'Injectable'],
+        },
+      ],
+    },
+    {
+      name: 'with alias and checkDestroy true',
+      code: stripIndent`
+        import { Component, OnDestroy } from "@angular/core";
+        import { of, switchMap, Subject } from "rxjs";
+
+        declare function someAlias(subject: unknown): unknown;
+
+        const observable = of("o");
+
+        @Component({
+          selector: "component-with-alias-checkdestroy-false"
+        })
+        class MyComponent extends OnDestroy {
+          private readonly destroy = new Subject<void>();
+
+          ngOnInit() {
+            observable.pipe(
+              switchMap(_ => observable),
+              someAlias(this.destroy.asObservable())
+            ).subscribe();
+          }
+          ngOnDestroy() {
+            // Alias doesn't enforce subject-based destroy.
+          }
+        }
+      `,
+      options: [{ alias: ['someAlias'], checkDestroy: true }],
+    },
+    {
       name: 'decorators with takeUntil',
       code: stripIndent`
         import { Component, OnDestroy } from "@angular/core";
@@ -669,6 +784,112 @@ ruleTester({ types: true }).run('prefer-takeuntil', preferTakeuntilRule, {
         }
       `,
       { options: [{ alias: ['someAlias'] }] },
+    ),
+    fromFixture(
+      'with alias accepting subject and checkDestroy true',
+      stripIndent`
+        import { Component } from "@angular/core";
+        import { of } from "rxjs";
+        import { switchMap } from "rxjs/operators";
+
+        declare function someAlias(subject: unknown): unknown;
+
+        const observable = of("o");
+
+        @Component({
+          selector: "component-with-alias-checkdestroy-true-no-ondestroy"
+        })
+        class NoDestroyComponent {
+              ~~~~~~~~~~~~~~~~~~ [noDestroy]
+          private readonly destroy = {};
+
+          ngOnInit() {
+            observable.pipe(
+              switchMap(_ => observable),
+              someAlias(this.destroy)
+            ).subscribe();
+          }
+        }
+      `,
+      { options: [{ alias: ['someAlias'], checkDestroy: true }] },
+    ),
+    fromFixture(
+      'with alias accepting this param and checkDestroy true',
+      stripIndent`
+        import { Component } from "@angular/core";
+        import { of } from "rxjs";
+        import { switchMap } from "rxjs/operators";
+
+        declare function untilDestroyed(instance: unknown): unknown;
+
+        const observable = of("o");
+
+        @Component({
+          selector: "component-with-alias-checkdestroy-true-this"
+        })
+        class NoTakeUntilComponent {
+              ~~~~~~~~~~~~~~~~~~~~ [noDestroy]
+          ngOnInit() {
+            observable.pipe(
+              switchMap(_ => observable),
+              untilDestroyed(this)
+            ).subscribe();
+          }
+        }
+      `,
+      { options: [{ alias: ['untilDestroyed'], checkDestroy: true }] },
+    ),
+    fromFixture(
+      'imported alias local name not configured',
+      stripIndent`
+        import { Component } from "@angular/core";
+        import { of } from "rxjs";
+        import { switchMap } from "rxjs/operators";
+
+        declare function ud(instance: unknown): unknown;
+
+        const observable = of("o");
+
+        @Component({
+          selector: "component-with-imported-local-alias"
+        })
+        class NoTakeUntilComponent {
+          ngOnInit() {
+            observable.pipe(
+              switchMap(_ => observable),
+              ud(this)
+            ).subscribe();
+              ~~~~~~~~~ [noTakeUntil]
+          }
+        }
+      `,
+      { options: [{ alias: ['untilDestroyed'] }] },
+    ),
+    fromFixture(
+      'without takeUntilDestroyed alias',
+      stripIndent`
+        import { Component, DestroyRef, inject } from "@angular/core";
+        import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+        import { of } from "rxjs";
+        import { switchMap } from "rxjs/operators";
+
+        const o = of("o");
+
+        @Component({
+          selector: "component-without-takeuntil-destroyed-alias"
+        })
+        class NoTakeUntilComponent {
+          private readonly destroyRef = inject(DestroyRef);
+
+          someMethod() {
+            o.pipe(
+              switchMap(_ => o)
+            ).subscribe();
+              ~~~~~~~~~ [noTakeUntil]
+          }
+        }
+      `,
+      { options: [{ alias: ['takeUntilDestroyed'] }] },
     ),
     fromFixture(
       'decorators without takeUntil',